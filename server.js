606d4a4ce404c6d7c817aadb1371e42ea0a17178
MC2023IT <134158079+MC2023IT@users.noreply.github.com>
2023-12-02 21:37:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -89,5 +89,3 @@ app.get('/latestImage', async (req, res) => {
 
 app.get('/client', (req, res) => res.sendFile(path.resolve(__dirname, './client.html')));
 app.listen(HTTP_PORT, () => console.log(`HTTP server listening at ${HTTP_PORT}`));
-                                                                                                                  63,1          57%
-                                                                                                                  1,1           Top
